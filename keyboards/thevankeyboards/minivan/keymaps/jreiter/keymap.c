f63b6e5e5fb7f2d378c4e07c4cd063669e925a84
jreiter <johnreiter@gmail.com>
2021-01-11 11:14:43 -0500

    Tweak minivan keymap

--- keyboards/thevankeyboards/minivan/keymaps/jreiter/keymap.c
+++ keyboards/thevankeyboards/minivan/keymaps/jreiter/keymap.c
@@ -28,29 +28,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |   Alt   |   GUI   |  Lower  |    TC/Space    |   TC/Space     |  Raise  |   Vol+  |  Play   |
  *  `--------+---------+---------+------^^^-------+-----^^^--------+---------+---------+---------'
  */
-[0] = LAYOUT(
-    KC_GESC,           KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,     KC_Y,   KC_U,    KC_I,     KC_O,    KC_P,     KC_BSPC,
-    KC_TAB,            KC_A,     KC_S,     KC_D,   KC_F,    KC_G,     KC_H,   KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_ENT,
-    TD(TD_LSFT_CAPS),  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,     KC_N,   KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  MO(3),
-    KC_LCTL,           KC_LALT,  KC_LGUI,  MO(1),  KC_SPC,  KC_RGUI,  MO(2),  KC_RCTL
+[0] = LAYOUT_arrow(
+    KC_GESC,           KC_Q,     KC_W,     KC_E,   KC_R,    KC_T,     KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,     KC_BSPC,
+    KC_TAB,            KC_A,     KC_S,     KC_D,   KC_F,    KC_G,     KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,  KC_ENT,
+    TD(TD_LSFT_CAPS),  KC_Z,     KC_X,     KC_C,   KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,  MO(3),
+    KC_LCTL,           KC_LALT,  KC_LGUI,  MO(1),  LT(1, KC_SPC),     KC_RGUI, KC_RALT, MO(2),  KC_RCTL
     ),
-[1] = LAYOUT(
+[1] = LAYOUT_arrow(
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     TD(TD_9_LBRC),  TD(TD_0_RBRC),  TD(TD_MINS_EQL),
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,       KC_QUOT,        KC_BSLS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,        KC_TRNS,        KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
-[2] = LAYOUT(
+[2] = LAYOUT_arrow(
     KC_TRNS,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
-[3] = LAYOUT(
+[3] = LAYOUT_arrow(
     RESET,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,
     KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
     )
 };
 
