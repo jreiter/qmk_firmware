6a0acea73c569aed869ab18074d1c3d0ea72f61f
jreiter <johnreiter@gmail.com>
2021-05-26 10:00:54 -0400

    Start using homerow mods on neuron

--- keyboards/walletburner/neuron/keymaps/jreiter/keymap.c
+++ keyboards/walletburner/neuron/keymaps/jreiter/keymap.c
@@ -17,13 +17,20 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM enter_combo[] = {KC_L, KC_SCLN, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {COMBO(enter_combo, KC_ENT)};
 
+#define KC_LSZ LSFT_T(KC_Z)
+#define KC_RSSL LSFT_T(KC_SLSH)
+#define KC_LCTF LCTL_T(KC_F)
+#define KC_RCTJ RCTL_T(KC_J)
+#define KC_LGD LGUI_T(KC_D)
+#define KC_RGK RGUI_T(KC_K)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(
-    KC_GESC,  KC_Q,    KC_W,   KC_E,  KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P  ,  KC_BSPC,
-    KC_TAB,   KC_A,    KC_S,   KC_D,  KC_F,   KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,
-    KC_LSFT,  KC_Z,    KC_X,   KC_C,  KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, LT(3, KC_SLSH),
-    KC_LCTL,  KC_LGUI, MO(1),  LT(2, KC_SPC), LT(2, KC_SPC),    KC_RALT, MO(2)
+    KC_GESC,  KC_Q,    KC_W,   KC_E,   KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P  ,  KC_BSPC,
+    KC_TAB,   KC_A,    KC_S,   KC_LGD, KC_LCTF,  KC_G,    KC_H,    KC_RCTJ, KC_RGK,  KC_L,   KC_SCLN,
+    KC_LSFT,  KC_LSZ,  KC_X,   KC_C,   KC_V,     KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT, KC_RSSL,
+    KC_LALT,  KC_LGUI, MO(1),  LT(2,   KC_SPC),  LT(2, KC_SPC),    MO(4),   MO(3)
   ),
 
   [1] = LAYOUT(
@@ -34,16 +41,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [2] = LAYOUT(
-    KC_TRNS,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_BSLS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_BSLS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
   [3] = LAYOUT(
-    RESET  ,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F5  ,  KC_F6  ,  KC_F7  ,  KC_F8  ,  KC_F9  ,  KC_F10 ,  KC_F11,
+    KC_TRNS,  KC_F1  ,  KC_F2  ,  KC_F3  ,  KC_F4  ,  KC_F5  ,  KC_F6  ,  KC_F7  ,  KC_F8  ,  KC_F9  ,  KC_F10 ,  KC_F11,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+  ),
+
+  [4] = LAYOUT(
+    RESET,    KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS,  KC_TRNS,  KC_VOLD,  KC_VOLU,  KC_MUTE,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   )
 };
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KC_LSZ:
+      return TAPPING_SHIFT_TERM;
+    case KC_RSSL:
+      return TAPPING_SHIFT_TERM;
+    default:
+      return TAPPING_TERM;
+  }
+}
